30d05702e3796420dfa43e5884e722c229846ddd
Ludo Pagie <l.pagie@nki.nl>
2019-09-09 10:51:03 +0200

    feat: add analysis for processing iPCR_67 data
    As it turned out the data generated by the first sequencing run was of
    too low quality for processing.

---
+++ analyses/LP190429_proc_iPCR_67/command-wrapper_iPCR-67_LP190429.sh
@@ -0,0 +1,18 @@
+DRYRUN=""
+# DRYRUN="--dryrun "
+
+DATETAG="LP$( date +"%y%m%d_%H%M" )"
+SNAKEFILE=pipeline/SuRE-snakemake
+CONFIG=config-SuRE42_1_LP181224.yml
+LOG="${CONFIG%.yml}_run-${DATETAG}.log"
+TARGET="bedpe_merged_smpls"
+TARGET="merged_ipcr_cdna"
+TARGET="reversed_liftover"
+TARGET="sorted_cnt_tbls"
+TARGET="bed2coverage_done"
+
+CMD="/usr/bin/time -v nice -19 snakemake "${DRYRUN}"-prs "${SNAKEFILE}" --use-conda --resources ram=150 --configfile "${CONFIG}" --cores 35 ${TARGET} &> "${LOG}""
+echo "${CMD}"
+eval ${CMD}
+
+
---
+++ analyses/LP190429_proc_iPCR_67/readme.md
@@ -0,0 +1,1 @@
+Applying the SuRE-INDEL pipeline to iPCR data set 67
