277a05a0c5a2efd931060f78912efa21bbd48c21
Ludo Pagie <l.pagie@nki.nl>
2019-10-17 11:12:17 +0200

    ana-dir; processing data

---
+++ analyses/LP20191017_processing_cDNA_iPCR/cmd_T01_LP20191017.sh
@@ -0,0 +1,21 @@
+DRYRUN=""
+# DRYRUN="--dryrun "
+
+DATETAG="LP$( date +"%Y%m%d_%H%M" )"
+SNAKEFILE=../../code/pipeline/SuRE-snakemake
+CONFIG=config-Dm12_T01_LP20191017.yml
+LOG="${CONFIG%.yml}_run-${DATETAG}.log"
+NCORES=30
+RAM=150
+TARGET="bedpe_merged_smpls"
+TARGET="merged_ipcr_cdna"
+TARGET="reversed_liftover"
+TARGET="sorted_cnt_tbls"
+TARGET=""
+# TARGET="bed2coverage_done"
+
+CMD="/usr/bin/time -v nice -19 snakemake ${DRYRUN}-prs ${SNAKEFILE} --use-conda --resources ram=${RAM} --configfile ${CONFIG} --cores $NCORES ${TARGET} &> ${LOG}"
+echo "${CMD}"
+eval ${CMD}
+
+
