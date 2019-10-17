b44f7f5ad38b3ecaec3bc3c48f80e128942fc8d1
Ludo Pagie <l.pagie@nki.nl>
2019-10-17 13:40:02 +0200

    T01 processing to target trim_cDNA

--- analyses/LP20191017_processing_cDNA_iPCR/cmd_T01_LP20191017.sh
+++ analyses/LP20191017_processing_cDNA_iPCR/cmd_T01_LP20191017.sh
@@ -11,7 +11,7 @@ TARGET="bedpe_merged_smpls"
 TARGET="merged_ipcr_cdna"
 TARGET="reversed_liftover"
 TARGET="sorted_cnt_tbls"
-TARGET=""
+TARGET="trim_cDNA"
 # TARGET="bed2coverage_done"
 
 CMD="/usr/bin/time -v nice -19 snakemake ${DRYRUN}-prs ${SNAKEFILE} --use-conda --resources ram=${RAM} --configfile ${CONFIG} --cores $NCORES ${TARGET} &> ${LOG}"
