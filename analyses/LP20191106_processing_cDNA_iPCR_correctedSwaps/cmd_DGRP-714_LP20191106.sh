20801e81c0783307bcb78a4ec5b836250a504790
Ludo Pagie <l.pagie@nki.nl>
2019-11-06 18:11:33 +0100

    all cmd wrappers with updated resource params

---
+++ analyses/LP20191106_processing_cDNA_iPCR_correctedSwaps/cmd_DGRP-714_LP20191106.sh
@@ -0,0 +1,24 @@
+DRYRUN=""
+# DRYRUN="--dryrun "
+
+DATETAG="LP$( date +"%Y%m%d_%H%M" )"
+SNAKEFILE=../../code/pipeline/SuRE-snakemake
+CONFIG=config-Dm06_DGRP-714_LP20191029.yml
+LOG="${CONFIG%.yml}_run-${DATETAG}.log"
+NCORES=15
+RAM=100
+TARGET="bedpe_merged_smpls"
+TARGET="merged_ipcr_cdna"
+TARGET="reversed_liftover"
+TARGET="trim_iPCR"
+TARGET="split_bam"
+TARGET="trim_iPCR"
+TARGET="sorted_cnt_tbls"
+#TARGET="bedpe_BC"
+# TARGET="bed2coverage_done"
+
+CMD="/usr/bin/time -v nice -19 snakemake ${DRYRUN}-prs ${SNAKEFILE} --use-conda --resources ram=${RAM} --configfile ${CONFIG} --cores $NCORES ${TARGET} &> ${LOG}"
+echo "${CMD}"
+eval ${CMD}
+
+
