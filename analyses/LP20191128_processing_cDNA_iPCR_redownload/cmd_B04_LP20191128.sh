c6248fa4c8a11918a2fd2c462a4ce26011b55915
Ludo Pagie <l.pagie@nki.nl>
2019-12-09 14:03:11 +0100

    rerun with redownloaded data

---
+++ analyses/LP20191128_processing_cDNA_iPCR_redownload/cmd_B04_LP20191128.sh
@@ -0,0 +1,24 @@
+DRYRUN=""
+# DRYRUN="--dryrun "
+
+DATETAG="LP$( date +"%Y%m%d_%H%M" )"
+SNAKEFILE=../../code/pipeline/SuRE-snakemake
+CONFIG=config-Dm08_B04_LP20191128.yml
+LOG="${CONFIG%.yml}_run-${DATETAG}.log"
+NCORES=15
+RAM=100
+TARGET="bedpe_merged_smpls"
+TARGET="merged_ipcr_cdna"
+TARGET="reversed_liftover"
+TARGET="sorted_cnt_tbls"
+TARGET="trim_iPCR"
+TARGET="split_bam"
+TARGET="bedpe"
+TARGET="sorted_cnt_tbls"
+# TARGET="bed2coverage_done"
+
+CMD="/usr/bin/time -v nice -19 snakemake ${DRYRUN}-prs ${SNAKEFILE} --use-conda --resources ram=${RAM} --configfile ${CONFIG} --cores $NCORES ${TARGET} &> ${LOG}"
+echo "${CMD}"
+eval ${CMD}
+
+
---
+++ analyses/LP20191128_processing_cDNA_iPCR_redownload/cmd_DGRP-324_LP20191128.sh
@@ -0,0 +1,25 @@
+DRYRUN=""
+# DRYRUN="--dryrun "
+
+DATETAG="LP$( date +"%Y%m%d_%H%M" )"
+SNAKEFILE=../../code/pipeline/SuRE-snakemake
+CONFIG=config-Dm03_DGRP-324_LP20191128.yml
+LOG="${CONFIG%.yml}_run-${DATETAG}.log"
+NCORES=15
+RAM=100
+TARGET="bedpe_merged_smpls"
+TARGET="merged_ipcr_cdna"
+TARGET="reversed_liftover"
+TARGET="trim_iPCR"
+TARGET="trim_iPCR"
+TARGET="sorted_cnt_tbls"
+TARGET="split_bam"
+TARGET="bedpe"
+# TARGET="bedpe_BC"
+# TARGET="bed2coverage_done"
+
+CMD="/usr/bin/time -v nice -19 snakemake ${DRYRUN}-prs ${SNAKEFILE} --use-conda --resources ram=${RAM} --configfile ${CONFIG} --cores $NCORES ${TARGET} &> ${LOG}"
+echo "${CMD}"
+eval ${CMD}
+
+
